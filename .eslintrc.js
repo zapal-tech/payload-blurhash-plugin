88e18c93457f02376ecb0708299d83fd7862237f
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2023-10-22 01:42:03 +0300

    feat: plugin environment and meta setup

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: ['./eslint-config'],
+  overrides: [
+    {
+      files: ['dev/**/*.ts'],
+      rules: {
+        'import/no-relative-packages': 'off',
+        'no-process-env': 'off',
+      },
+    },
+  ],
+  // excludes: ['dev/plugin.spec.ts'],
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# Payload CMS BlurHash Plugin by Zapal
+
+[![](/github-banner.jpg)](https://www.zapal.tech)
+
+## 🛡️ License
+
+This project is licensed under the [MIT License](/LICENSE)
+
+## ⭐ Like what we're doing? Give us a star
